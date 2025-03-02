d458e46acf3c7dc3e8329c95263157c6c9aa4edf
Love Singhal <groothoon31@gmail.com>
2025-03-03 01:19:30 +0530

    react flow

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,6 +1,11 @@
+import Chai from "./Chai";
+
 function App() {
   return (
-    <h1>Hello World! | learned how to create a react app through npm npx and vite</h1>
+    <>
+    <Chai/>
+   {/* <h1>Hello World!</h1>  */}
+    </>
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,10 @@
+import Chai from "./chai"
+
+
+
 function App() {
   return (
-    <h1>Hello World! with vite | 31</h1>
+    <Chai/>
   )
 }
 
