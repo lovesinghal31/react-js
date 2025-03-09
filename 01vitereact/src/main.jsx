7b1cf5414932307a4ebf2b2648c64b2c97d16b9d
Love Singhal <groothoon31@gmail.com>
2025-03-10 00:29:01 +0530

    custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom Appdfdfd!</h1>
+        </div>
+    );
+}
+
+
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const anotherElement = (
+    <a href="https://google.com" target="_blank">Vist Google</a>
+)
+
 createRoot(document.getElementById('root')).render(
     <App />
+    // ReactElement
+    // anotherElement
 )
