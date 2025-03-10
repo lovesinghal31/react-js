a7f630649c6c1458c08fba84cf769571cb5d35f3
Love Singhal <groothoon31@gmail.com>
2025-03-11 00:25:58 +0530

    tailwindcss

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './Footer'
+import Footer from './Footer'
+import Content from './Content'
+import Carousel from './Carousel'
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+      <Content />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ 03tailwindprops/src/Carousel.jsx
@@ -0,0 +1,44 @@
+function Carousel() {
+    return (
+        <div className="carousel w-full h-75">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img
+                    src="https://plus.unsplash.com/premium_photo-1672762542894-caaa8d4f0a77?q=80&w=1971&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    className="w-full object-cover" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Carousel
