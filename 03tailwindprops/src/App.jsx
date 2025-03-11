712c146c754048459b933ade79ba48a16d91b309
Love Singhal <groothoon31@gmail.com>
2025-03-11 14:48:48 +0530

    tailwind and props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,19 +1,28 @@
-import { useState } from 'react'
-import './Footer'
-import Footer from './Footer'
-import Content from './Content'
-import Carousel from './Carousel'
-import Navbar from './Navbar'
-
+import { useState } from "react";
+import Footer from "./Footer";
+import Content from "./Content";
+import Carousel from "./Carousel";
+import Navbar from "./Navbar";
+import Card from "./components/Card";
 function App() {
+  let someObj = {
+    userName: "lovesinghal31",
+    age: 18
+  }
+  
+  let arr =[1,2,3,4]
+
+
   return (
     <>
-      <Navbar />
-      <Carousel />
-      <Content />
-      <Footer />
+      <h1 className="text-mint-500 text-try animate-pulse text-center items-center flex justify-center object-center">
+        Hello
+      </h1>
+      <Card productName="NIKE" btnText="Kharid le" />
+      <Card productName="PUMA" btnText="Purchase" />
+      <Card />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
