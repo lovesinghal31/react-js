47e1cf277e9fe782298d415da6f114844bc644f0
Love Singhal <groothoon31@gmail.com>
2025-04-28 17:35:40 +0530

    input form changes in todolist

--- 10todoContextLocal/src/components/TodoFrom.jsx
+++ 10todoContextLocal/src/components/TodoFrom.jsx
@@ -20,9 +20,9 @@ function TodoForm() {
                 placeholder="Write Todo..."
                 value={todo}
                 onChange={(e)=>setTodo(e.target.value)}
-                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5 input-field"
             />
-            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0 add-button">
                 Add
             </button>
         </form>
